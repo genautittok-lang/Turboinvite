756f5ba4b951acfbbb2eed823e22168347da0f0d
genautittok-lang <genautittok@gmail.com>
2025-11-17 14:39:20 +0200

    support.py

---
+++ handlers/support.py
@@ -0,0 +1,14 @@
+# handlers/support.py
+from aiogram import Router
+from aiogram.types import CallbackQuery
+from texts import texts
+
+router = Router()
+
+@router.callback_query(lambda c: c.data == "support")
+async def support(query: CallbackQuery):
+    user_id = query.from_user.id
+    # Мова користувача (як приклад беремо українську якщо не знайдено)
+    lang = 'uk'
+    txt = texts[lang]['support']
+    await query.message.answer(txt)
