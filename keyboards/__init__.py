fd5a7777a44041418f2881b3c540227ce3296eb4
genautittok-lang <genautittok@gmail.com>
2025-11-17 18:03:34 +0200

    __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,16 +1,1 @@
-from .start import register_start
-from .referral import register_referral
-from .earnings import register_earnings
-from .withdraw import register_withdraw
-from .promotion import register_promotion
-from .stars import register_stars
-from .admin import register_admin
-
-def register_all(dp):
-    register_start(dp)
-    register_referral(dp)
-    register_earnings(dp)
-    register_withdraw(dp)
-    register_promotion(dp)
-    register_stars(dp)
-    register_admin(dp)
+# Пакет keyboards
