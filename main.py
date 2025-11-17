2559fdc75107a4ecad2842172e18f226cc72c2a5
genautittok-lang <genautittok@gmail.com>
2025-11-17 13:40:16 +0200

    main.py

--- main.py
+++ main.py
@@ -3,18 +3,13 @@ from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from handlers import register_all
 
-if not BOT_TOKEN:
-    raise RuntimeError("BOT_TOKEN env var not set")
-
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+
 register_all(dp)
 
 async def main():
-    try:
-        await dp.start_polling(bot)
-    except Exception as e:
-        print("Polling error:", e)
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
