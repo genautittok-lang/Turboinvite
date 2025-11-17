1dbf2ae1308de973292be01d66db56f49a1c9dcb
genautittok-lang <genautittok@gmail.com>
2025-11-17 18:11:05 +0200

    __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,16 +1,20 @@
-from .start import register_start
-from .referral import register_referral
-from .earnings import register_earnings
-from .withdraw import register_withdraw
-from .promotion import register_promotion
-from .stars import register_stars
-from .admin import register_admin
+# handlers/__init__.py
+from .start import router as start_router
+from .referral import router as referral_router
+from .earnings import router as earnings_router
+from .withdraw import router as withdraw_router
+from .promotion import router as promotion_router
+from .stars import router as stars_router
+from .admin import router as admin_router
 
 def register_all(dp):
-    register_start(dp)
-    register_referral(dp)
-    register_earnings(dp)
-    register_withdraw(dp)
-    register_promotion(dp)
-    register_stars(dp)
-    register_admin(dp)
+    """
+    Реєструє всі роутери в Dispatcher
+    """
+    dp.include_router(start_router)
+    dp.include_router(referral_router)
+    dp.include_router(earnings_router)
+    dp.include_router(withdraw_router)
+    dp.include_router(promotion_router)
+    dp.include_router(stars_router)
+    dp.include_router(admin_router)
