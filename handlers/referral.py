ed5a7443c3f9a79ec971330e6ced5ff76e511dc5
genautittok-lang <genautittok@gmail.com>
2025-11-17 19:22:37 +0200

    referral.py

--- handlers/referral.py
+++ handlers/referral.py
@@ -1,13 +1,17 @@
 from aiogram.types import CallbackQuery
 from config import BOT_USERNAME
 from texts import TEXTS
-from utils import get_user
+from utils import get_user  # функція повинна повертати об'єкт користувача з його мовою
 
 def register_referral(dp):
     @dp.callback_query(lambda c: c.data == "earn")
     async def referral_handler(call: CallbackQuery):
-        user = get_user(call.from_user.id)
+        user = get_user(call.from_user.id)  # повертає об'єкт користувача
         link = f"https://t.me/{BOT_USERNAME}?start={call.from_user.id}"
-        text = TEXTS["referral_link"]["EN"].format(link=link)
+
+        # Беремо мову користувача або англійську за замовчуванням
+        lang = getattr(user, "language", "EN").upper()
+        text = TEXTS["referral_link"].get(lang, TEXTS["referral_link"]["EN"]).format(link=link)
+
         await call.message.answer(text)
         await call.answer()
